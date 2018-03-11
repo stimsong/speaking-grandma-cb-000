dcde1e3ba666f0ee9ad687b420ceb340ff24bc1d
Greg Stimson <stimsong@yahoo.com>
2018-03-11 23:02:31 +0000

    automatically backed up by learn

--- grandma.rb
+++ grandma.rb
@@ -15,5 +15,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     "I LOVE YOU TOO PUMPKIN!"
+  elsif phrase != phrase.uppercase
+    "HUH?! SPEAK UP, SONNY!"
   else end
 end
